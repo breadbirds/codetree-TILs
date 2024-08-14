87ccb19c8afcbf1735eff110768153f1d3f843be
yujin <161617508+breadbirds@users.noreply.github.com>
2024-08-15 01:45:16 +0900

    Title: 두 사람, Time: 136ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240815/두 사람/two-person.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int aAge, bAge;
+        char aGender, bGender;
+
+        aAge = sc.nextInt();
+        aGender = sc.next().charAt(0);
+        bAge = sc.nextInt();
+        bGender = sc.next().charAt(0);
+
+        if((aAge >= 19 || bAge >= 19) && (aGender == 'M' || bGender == 'M') ){
+            System.out.print("1");
+        }
+        else{
+            System.out.print("0");
+        }
+
+
+    }
+}
