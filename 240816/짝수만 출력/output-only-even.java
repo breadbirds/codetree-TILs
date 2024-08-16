44da4f063ef96748505055882d48a965cb102303
yujin <161617508+breadbirds@users.noreply.github.com>
2024-08-16 10:30:11 +0900

    Title: 짝수만 출력, Time: 142ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240816/짝수만 출력/output-only-even.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = 0;
+
+        while(c >= a && c <= b){
+            System.out.print(c + " ");
+            c += 2;
+        }
+    }
+}
