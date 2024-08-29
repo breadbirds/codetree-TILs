e3056197a2df4fbe3f1ee64601519d891bec93d0
yujin <161617508+breadbirds@users.noreply.github.com>
2024-08-29 12:50:19 +0900

    Title: 조건에 따른 연산, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240829/조건에 따른 연산/operatino-by-rule.java
+++ 240829/조건에 따른 연산/operatino-by-rule.java
@@ -8,19 +8,20 @@ public class Main {
         int cnt = 0;
 
         while (true) {
-            cnt++;
+           
             if(n >= 1000) {
                 
                 break;
             }
             if(n % 2 == 0) {
-                n *= 3 + 1;
+                n = (n *= 3) + 1;
+               
                 
             }
             else {
-                n *= 2 + 2;
+                n = (n *= 2) + 2;
             }
-          
+           cnt++;
         }
         System.out.println(cnt);
     }
